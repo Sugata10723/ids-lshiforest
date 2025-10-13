8871a5d29ca104bd03251f65cc29f2b4492102a9
Sugata10723 <ikaikaga308@gmail.com>
2025-10-13 15:40:39 +0900

    update datalodar to run pca for numeerical data.

--- minhash/data_loader.py
+++ minhash/data_loader.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 from typing import Tuple
+import numpy.typing as npt
 import config
 
+# List of main function
 # load_unsw_for_minhash
 # load_nsl_for_minhash
 # load_unsw_binned
@@ -32,8 +35,8 @@ def load_unsw_for_minhash() -> (
     X_train = X_train[config.categorical_columns_unsw]
     X_test = X_test[config.categorical_columns_unsw]
 
-    X_train_minhash = process_for_minhash(X_train)
-    X_test_minhash = process_for_minhash(X_test)
+    X_train_minhash = _process_for_minhash(X_train)
+    X_test_minhash = _process_for_minhash(X_test)
 
     return X_train_minhash, y_train, X_test_minhash, y_test, y_cat
 
@@ -58,8 +61,8 @@ def load_nsl_for_minhash() -> Tuple[pd.Series, pd.Series, pd.Series, pd.Series]:
     X_train = X_train[config.categorical_columns_nsl]
     X_test = X_test[config.categorical_columns_nsl]
 
-    X_train_minhash = process_for_minhash(X_train)
-    X_test_minhash = process_for_minhash(X_test)
+    X_train_minhash = _process_for_minhash(X_train)
+    X_test_minhash = _process_for_minhash(X_test)
 
     return X_train_minhash, y_train, X_test_minhash, y_test
 
@@ -81,45 +84,26 @@ def load_unsw_binned() -> Tuple[pd.Series, pd.Series, pd.Series, pd.Series, pd.S
     X_train = train_df.drop("label", axis=1)
     X_test = test_df.drop("label", axis=1)
 
-    X_train_binned = X_train.copy()
-    X_test_binned = X_test.copy()
-
-    sc = StandardScaler()
-    X_train_bs = pd.DataFrame(
-        sc.fit_transform(X_train_binned[config.numerical_columns_unsw]),
-        columns=config.numerical_columns_unsw,
-    )  # bs = binned scaled
-    X_test_bs = pd.DataFrame(
-        sc.transform(X_test_binned[config.numerical_columns_unsw]),
-        columns=config.numerical_columns_unsw,
+    # Run standard scaler and PCA to numerical data
+    X_train_bs, X_test_bs = _process_sc_pca(
+        X_train_num=X_train[config.numerical_columns_unsw],
+        X_test_num=X_test[config.numerical_columns_unsw],
+        num_column_names=config.numerical_columns_unsw,
+        n_pca=5,
     )
 
-    for col in config.numerical_columns_unsw:
-        binned_series, bin_edges = pd.cut(
-            X_train_bs[col],
-            bins=10,
-            retbins=True,
-            labels=False,
-            duplicates="drop",
-        )
-
-        X_train_bs[col] = binned_series
-        X_test_bs[col] = pd.cut(
-            X_test_bs[col], bins=bin_edges, labels=False, include_lowest=True
-        )
-        X_test_bs[col] = X_test_bs[col].fillna(-1).astype(int)
-
-    # Reset Index (add 2025/09/16)
-    X_train_categorical = X_train[config.categorical_columns_unsw].reset_index(
-        drop=True
+    # Reset categorical columns index and concatenate them.
+    X_train_comed = pd.concat(
+        [X_train[config.categorical_columns_unsw].reset_index(drop=True), X_train_bs],
+        axis=1,
     )
-    X_test_categorical = X_test[config.categorical_columns_unsw].reset_index(drop=True)
+    X_test_comed = pd.concat(
+        [X_test[config.categorical_columns_unsw].reset_index(drop=True), X_test_bs],
+        axis=1,
+    )
 
-    X_train_comed = pd.concat([X_train_categorical, X_train_bs], axis=1)
-    X_test_comed = pd.concat([X_test_categorical, X_test_bs], axis=1)
-
-    X_train_minhash = process_for_minhash(X_train_comed)
-    X_test_minhash = process_for_minhash(X_test_comed)
+    X_train_minhash = _process_for_minhash(X_train_comed)
+    X_test_minhash = _process_for_minhash(X_test_comed)
 
     return X_train_minhash, y_train, X_test_minhash, y_test, y_cat
 
@@ -140,47 +124,76 @@ def load_nsl_binned() -> Tuple[pd.Series, pd.Series, pd.Series, pd.Series]:
     X_train = train_df.drop(["class", "difficulty"], axis=1)
     X_test = test_df.drop(["class", "difficulty"], axis=1)
 
-    sc = StandardScaler()
-    X_train_bs = pd.DataFrame(
-        sc.fit_transform(X_train[config.numerical_columns_nsl]),
-        columns=config.numerical_columns_nsl,
-    )  # bs = binned scaled
-    X_test_bs = pd.DataFrame(
-        sc.transform(X_test[config.numerical_columns_nsl]),
-        columns=config.numerical_columns_nsl,
+    # process standard scaler and pca to numerical data
+    X_train_bs, X_test_bs = _process_sc_pca(
+        X_train_num=X_train[config.numerical_columns_nsl],
+        X_test_num=X_test[config.numerical_columns_nsl],
+        num_column_names=config.numerical_columns_nsl,
+        n_pca=5,
     )
 
-    for col in config.numerical_columns_nsl:
+    X_train_comed = pd.concat(
+        [X_train_bs, X_train[config.categorical_columns_nsl].reset_index(drop=True)],
+        axis=1,
+    )
+    X_test_comed = pd.concat(
+        [X_test_bs, X_test[config.categorical_columns_nsl].reset_index(drop=True)],
+        axis=1,
+    )
+
+    X_train_minhash = _process_for_minhash(X_train_comed)
+    X_test_minhash = _process_for_minhash(X_test_comed)
+
+    return X_train_minhash, y_train, X_test_minhash, y_test
+
+
+def _process_for_minhash(df: pd.DataFrame) -> pd.Series:  # よりよい方法あるかもしれない
+    return df.astype(str).apply(
+        lambda row: {f"{col}:{val}" for col, val in row.items()}, axis=1
+    )
+
+
+def _process_sc_pca(
+    X_train_num: pd.DataFrame,
+    X_test_num: pd.DataFrame,
+    num_column_names: list,
+    n_pca: int = 5,
+) -> Tuple[pd.DataFrame, pd.DataFrame]:
+
+    sc = StandardScaler()
+    X_train_ss: pd.DataFrame = pd.DataFrame(
+        sc.fit_transform(X_train_num),
+        columns=num_column_names,
+    )
+    X_test_ss: pd.DataFrame = pd.DataFrame(
+        sc.transform(X_test_num),
+        columns=num_column_names,
+    )
+
+    pca = PCA(n_components=n_pca)
+    _column_pca = [f"pca_{i+1}" for i in range(n_pca)]
+    _train_pca: npt.ArrayLike = pca.fit_transform(X_train_ss)
+    _test_pca: npt.ArrayLike = pca.transform(X_test_ss)
+    X_train_pca: pd.DataFrame = pd.DataFrame(
+        data=_train_pca, columns=_column_pca
+    )  # Reset index
+    X_test_pca: pd.DataFrame = pd.DataFrame(data=_test_pca, columns=_column_pca)
+
+    X_train_bs: pd.DataFrame = X_train_pca.copy()
+    X_test_bs: pd.DataFrame = X_test_pca.copy()
+    for col in X_test_pca:
         binned_series, bin_edges = pd.cut(
-            X_train_bs[col],
+            X_train_pca[col],
             bins=10,
             retbins=True,
             labels=False,
             duplicates="drop",
         )
-
         X_train_bs[col] = binned_series
+
         X_test_bs[col] = pd.cut(
-            X_test_bs[col], bins=bin_edges, labels=False, include_lowest=True
+            X_test_pca[col], bins=bin_edges, labels=False, include_lowest=True
         )
         X_test_bs[col] = X_test_bs[col].fillna(-1).astype(int)
 
-    X_train_comed = pd.concat(
-        [X_train_bs, X_train[config.categorical_columns_nsl]], axis=1
-    )
-    X_test_comed = pd.concat(
-        [X_test_bs, X_test[config.categorical_columns_nsl]], axis=1
-    )
-
-    X_train_minhash = process_for_minhash(X_train_comed)
-    X_test_minhash = process_for_minhash(X_test_comed)
-
-    return X_train_minhash, y_train, X_test_minhash, y_test
-
-
-# convert x: pd.DataFrame to set
-# よりよい方法あるかもしれない
-def process_for_minhash(df: pd.DataFrame) -> pd.Series:
-    return df.astype(str).apply(
-        lambda row: {f"{col}:{val}" for col, val in row.items()}, axis=1
-    )
+    return X_train_bs, X_test_bs
